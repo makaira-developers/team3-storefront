46a35c1bb66cac48e204524df6a10a76addd8c0c
hkreuter <heike.reuter@oxid-esales.com>
2022-01-21 10:34:30 +0100

    Update queries.js

--- utils/oxid/Checkout/querys.js
+++ utils/oxid/Checkout/querys.js
@@ -26,7 +26,43 @@ const BASKET_COST_QUERYPART = gql`
   }
 `
 
-const BASKET_CONTENT_QUERYPART = gql`
+const BASKET_DELIVERY_QUERYPART = gql`
+  deliveryMethod {
+    id
+    title
+  }
+`
+
+const BASKET_PAYMENT_QUERYPART = gql`
+  payment{
+    id
+    title
+  }
+`
+
+const INVOICEADDRESS_QUERYPART = gql`
+  invoiceAddress {
+    salutation
+    firstName
+    lastName
+    street
+    streetNumber
+    zipCode
+    city
+    country {
+      isoAlpha2
+    }
+  }
+`
+
+const BASKET_CONTENTS_QUERYPART = gql`
+  ${BASKET_COST_QUERYPART}
+  ${ITEMS_QUERYPART}
+`
+
+const BASKET_EXTENDED_QUERYPART = gql`
+  ${BASKET_DELIVERY_QUERYPART}
+  ${BASKET_PAYMENT_QUERYPART}
   ${BASKET_COST_QUERYPART}
   ${ITEMS_QUERYPART}
 `
@@ -43,10 +79,7 @@ const QUERY_BASKET = gql`
   query singleBasket($basketId: ID!) {
     basket(basketId: $basketId) {
       id
-      owner {
-        firstName
-      }
-      ${BASKET_CONTENT_QUERYPART}
+      ${BASKET_EXTENDED_QUERYPART}
     }
   }
 `
@@ -55,8 +88,74 @@ const ADD_TO_BASKET = gql`
   mutation addItem($productId: ID!, $basketId: ID!, $amount: Float!) {
     basketAddItem(basketId: $basketId, productId: $productId, amount: $amount) {
       id
-      ${BASKET_CONTENT_QUERYPART}
+      ${BASKET_CONTENTS_QUERYPART}
   }
 `
 
-export { ADD_TO_BASKET, QUERY_BASKET, CREATE_BASKET }
+const REMOVE_FROM_BASKET = gql`
+  mutation basketRemoveItem ($basketId: ID!, $basketItemId: ID!, $amount: Float!) {
+    basketRemoveItem (
+      basketId: $basketId
+      basketItemId: $basketItemId
+      amount: $amount
+    ) {
+      id
+      ${BASKET_CONTENTS_QUERYPART}
+    }
+  }
+`
+
+const FETCH_TOKEN = gql`
+  query fetchToken ($userName: String!, $password: String!) {
+    token (
+      username: $userName
+      password: $password
+    )  
+  }
+`
+const PLACE_ORDER = gql`
+  mutation placeOrder ($basketId: ID!){
+    placeOrder(
+      basketId: $basketId
+      confirmTermsAndConditions: true
+    ) {
+      id
+      orderNumber
+    }
+ }
+`
+
+const CUSTOMER_DETAILS = gql`
+  query customerDetails {
+   customer {
+      id
+      email
+      ${INVOICEADDRESS_QUERYPART}
+  }
+}
+`
+
+const SET_DELIVERY_METHOD = gql`
+  mutation shippingMethod ($basketId: ID!, $deliveryMethodId: ID!) {
+    basketSetDeliveryMethod (
+      basketId: $basketId
+      deliveryMethodId: $deliveryMethodId
+    ) {
+      id
+      ${BASKET_EXTENDED_QUERYPART}
+    }
+ }
+`
+
+const SET_PAYMENT_METHOD = gql`
+  mutation paymentMethod ($basketId: ID!, $paymentId: ID!) {
+    basketSetPayment (
+      basketId: $basketId
+      deliveryMethodId: $deliveryMethodId
+    ) {
+      id
+      ${BASKET_EXTENDED_QUERYPART}
+    }
+ }
+`
+export { FETCH_TOKEN, CUSTOMER_DETAILS, CREATE_BASKET, ADD_TO_BASKET, REMOVE_FROM_BASKET, QUERY_BASKET, SET_DELIVERY_METHOD, SET_PAYMENT_METHOD, PLACE_ORDER}
